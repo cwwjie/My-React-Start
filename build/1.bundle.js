3d87bf2e33ee7d30b1dfcd08003a1a7fb9ad6748
cwwjie <454766952@qq.com>
2016-12-17 10:57:06 +0800

    First-go
    最初版本(以后还会改进)

---
+++ build/1.bundle.js
@@ -0,0 +1,10 @@
+webpackJsonp([1],{
+
+/***/ 4:
+/***/ function(module, exports) {
+
+	console.log("c")
+
+/***/ }
+
+});
---
+++ build/2.bundle.js
@@ -0,0 +1,10 @@
+webpackJsonp([2],{
+
+/***/ 5:
+/***/ function(module, exports) {
+
+	console.log("d")
+
+/***/ }
+
+});
